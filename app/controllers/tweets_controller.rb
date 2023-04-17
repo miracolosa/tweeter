1e121b3961b6b8d5a5fc067c7f481b9c0ca53bbc
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-17 16:21:41 +0200

    complete create method in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,29 @@
 class TweetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: %i[index]
 
   def index
     @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html { redirect_to root_path }
+      end
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+
 end
