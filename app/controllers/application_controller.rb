80ffaa5db6955a8ee054224a2d297f400db3f209
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-18 16:00:13 +0200

    fix issue with tweet partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
-
+  layout :layout_by_resource
   before_action :authenticate_user!
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   connect() {
   }
 
-  resetForm(event) {
-    event.target.reset()
+  resetForm() {
+    this.element.reset()
   }
 }
