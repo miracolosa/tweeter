877666f7b296d698596cb5430793d4990d5c39e6
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-19 13:59:00 +0200

    create tests for tweet validations

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    describe 'body' do
+      it 'must be present' do
+        tweet = described_class.new
+        expect(tweet).to_not be_valid
+      end
+
+      it "mustn't have more than 140 characters" do
+        tweet = described_class.new(body: "This is a very long string of characters that at some point will reach more
+          than 140 characters and should then not be valid as a tweet if the validations are correct.")
+        expect(tweet).to_not be_valid
+      end
+    end
+
+    describe 'user' do
+      it 'must be attached' do
+        tweet = described_class.new(body: "This is a tweet")
+        expect { tweet.save! }.to raise_error("Validation failed: User must exist")
+      end
+    end
+  end
+
 end
