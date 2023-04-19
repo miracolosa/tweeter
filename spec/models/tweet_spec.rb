d5ba6a133ca94e095e4f3a76ebcba4218c70a265
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-19 14:10:17 +0200

    refactor tweet test

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
+  subject do
+    user = User.create(email: "test@gmail.com", password: "topsecret", password_confirmation: "topsecret")
+    described_class.new(body: "This is a tweet", user:)
+  end
+
   describe "validations" do
     describe 'body' do
       it 'must be present' do
-        tweet = described_class.new
-        expect(tweet).to_not be_valid
+        subject.body = nil
+        expect(subject).to_not be_valid
       end
 
       it "mustn't have more than 140 characters" do
-        tweet = described_class.new(body: "This is a very long string of characters that at some point will reach more
-          than 140 characters and should then not be valid as a tweet if the validations are correct.")
-        expect(tweet).to_not be_valid
+        subject.body = "This is a very long string of characters that at some point will reach more
+          than 140 characters and should then not be valid as a tweet if the validations are correct."
+        expect(subject).to_not be_valid
       end
     end
 
     describe 'user' do
       it 'must be attached' do
-        tweet = described_class.new(body: "This is a tweet")
-        expect { tweet.save! }.to raise_error("Validation failed: User must exist")
+        subject.user = nil
+        expect { subject.save! }.to raise_error("Validation failed: User must exist")
       end
     end
   end
