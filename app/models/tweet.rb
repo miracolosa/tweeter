3fe8e05c938798423912cd2a3748ec720aec29fb
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-24 18:45:24 +0200

    add time difference to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,15 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: { maximum: 240 }
+
+  def time_difference
+    difference = Time.current - self.created_at
+    if (difference / 1.hour).round > 24
+      return self.created_at.strftime("%b %d, %Y")
+    elsif (difference / 1.minute).round > 120
+      return "#{(difference / 1.hour).round}h"
+    else
+      return "#{(difference / 1.minute).round}m"
+    end
+  end
 end
