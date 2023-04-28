0dcaf4b887fc2dfcc7675a85030e696058cbadf7
Milagros Lasarte <lasartemilagros@gmail.com>
2023-04-28 10:14:15 +0200

    add links to tweet show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,32 @@
 class CommentsController < ApplicationController
-
-  def created
+  def create
+    @comment = Comment.new(comment_params)
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment.tweet = @tweet
+    @comment.user = current_user
+    format_response
   end
 
   def destroy
   end
+
+  private
+
+  def format_response
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = "Body can't be blank."
+          redirect_to tweet_path(@tweet)
+        end
+      end
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
